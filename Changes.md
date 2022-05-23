db58a0a78a1a126cc00f4f0d48c464d1d20e39e1
Matt Simerson <matt@tnpi.net>
2022-05-23 12:07:00 -0700

    style: async/await where possible (#19)
    - dep: remove async
    - use builtin/promises where available
    - asn.ini: switch default dns provider to rspamd

--- Changes.md
+++ Changes.md
@@ -1,4 +1,21 @@
 
+
+## 2.0.0 - 2022-05-23
+
+- style: replace most callbacks with async/await
+- use builtin/promises where available
+
+
+## 1.0.9 - 2022-05-22
+
+- ci: add GitHub Actions CI, #17
+- ci: remove appveyor and travis configs
+- lint: prefer-template
+- test: wait longer for dns test
+- style: more es6/7
+- dep(async): bump version to 3.2
+
+
 ## 1.0.8 - 2018-01-22
 
 - parse maxmind ASN w/o Org data
@@ -7,15 +24,18 @@
 - added parse_rspamd test
 - emit rspamd DNS provider results (when enabled)
 
+
 ## 1.0.7 - 2017-02-06
 
 - updated eslint to use eslint-plugin-haraka
 - aggregate results before emitting
 
+
 ## 1.0.6 - 2016-10-20
 
 * when protocols[setting]=false, don't enable that protocol
     * ie, do what the config implies
+
 
 ## 1.0.5 - 2016-10-08
 
--- test/asn.js
+++ test/asn.js
@@ -14,7 +14,7 @@ describe('parse_monkey', function () {
     assert.deepEqual(
       asn.parse_monkey('74.125.44.0/23 | AS15169 | Google Inc. | 2000-03-30'),
       { net: '74.125.44.0/23', asn: '15169', org: 'Google Inc.',
-        date: '2000-03-30', country: undefined
+        date: '2000-03-30', country: undefined,
       }
     )
   })
@@ -23,7 +23,7 @@ describe('parse_monkey', function () {
     assert.deepEqual(
       asn.parse_monkey('74.125.0.0/16 | AS15169 | Google Inc. | 2000-03-30 | US'),
       { net: '74.125.0.0/16', asn: '15169', org: 'Google Inc.',
-        date: '2000-03-30', country: 'US'
+        date: '2000-03-30', country: 'US',
       }
     )
   })
@@ -44,22 +44,21 @@ describe('parse_routeviews', function () {
     assert.deepEqual(
       asn.parse_routeviews('40431 208.75.176.0 21'),
       {
-        asn: '40431', net: '208.75.176.0/21'
+        asn: '40431', net: '208.75.176.0/21',
       }
     );
   });
 
   it('15169 CSV string', function () {
     assert.deepEqual(
-      asn.parse_routeviews('15169,8.8.8.0,24'),
-      {asn: '15169', net: '8.8.8.0/24'}
+      asn.parse_routeviews('15169,8.8.8.0,24'), { asn: '15169', net: '8.8.8.0/24' }
     )
   })
 
   it('40431 array', function () {
     assert.deepEqual(
       asn.parse_routeviews(['40431','208.75.176.0','21']),
-      {asn: '40431', net: '208.75.176.0/21' }
+      { asn: '40431', net: '208.75.176.0/21' }
     )
   })
 })
@@ -72,7 +71,7 @@ describe('parse_cymru', function () {
     assert.deepEqual(
       asn.parse_cymru('40431 | 208.75.176.0/21 | US | arin | 2007-03-02'),
       { asn: '40431', net: '208.75.176.0/21', country: 'US',
-        assignor: 'arin', date: '2007-03-02'
+        assignor: 'arin', date: '2007-03-02',
       }
     );
   });
@@ -81,7 +80,7 @@ describe('parse_cymru', function () {
     assert.deepEqual(
       asn.parse_cymru('10290 | 12.129.48.0/24 | US | arin |'),
       { asn: '10290', net: '12.129.48.0/24', country: 'US',
-        assignor: 'arin', date: ''
+        assignor: 'arin', date: '',
       }
     )
   })
@@ -96,7 +95,7 @@ describe('parse_rspamd', function () {
       asn.parse_rspamd('15169|8.8.8.0/24|US|arin|'),
       {
         asn: '15169', net: '8.8.8.0/24', country: 'US',
-        assignor: 'arin', date: ''
+        assignor: 'arin', date: '',
       }
     )
   })
@@ -110,9 +109,8 @@ describe('get_dns_results', function () {
 
   it('origin.asn.cymru.com', function (done) {
     this.timeout(4000)
-    asn.get_dns_results('origin.asn.cymru.com', '8.8.8.8', function (err, zone, obj) {
+    asn.get_dns_results('origin.asn.cymru.com', '8.8.8.8').then(obj => {
       if (obj) {
-        assert.equal('origin.asn.cymru.com', zone);
         assert.equal('15169', obj.asn);
         assert.equal('8.8.8.0/24', obj.net);
       }
@@ -120,13 +118,12 @@ describe('get_dns_results', function () {
         assert.equal('something', obj);
       }
       done();
-    });
-  });
+    })
+  })
 
   it('asn.routeviews.org', function (done) {
-    asn.get_dns_results('asn.routeviews.org', '8.8.8.8', function (err, zone, obj) {
+    asn.get_dns_results('asn.routeviews.org', '8.8.8.8').then(obj => {
       if (obj) {
-        assert.equal('asn.routeviews.org', zone);
         if (obj.asn && obj.asn === '15169') {
           assert.equal('15169', obj.asn);
         }
@@ -140,9 +137,8 @@ describe('get_dns_results', function () {
 
   it('asn.rspamd.com', function (done) {
     this.timeout(3000);
-    asn.get_dns_results('asn.rspamd.com', '8.8.8.8', function (err, zone, obj) {
+    asn.get_dns_results('asn.rspamd.com', '8.8.8.8').then((obj, zone) => {
       if (obj) {
-        assert.equal('asn.rspamd.com', zone);
         assert.equal('15169', obj.asn);
         assert.equal('8.8.8.0/24', obj.net);
       }
@@ -155,9 +151,8 @@ describe('get_dns_results', function () {
 
   it('origin.asn.spameatingmonkey.net', (done) => {
     this.timeout(3000);
-    asn.get_dns_results('origin.asn.spameatingmonkey.net', '8.8.8.8', (err, zone, obj) => {
+    asn.get_dns_results('origin.asn.spameatingmonkey.net', '8.8.8.8').then((obj, zone) => {
       if (obj) {
-        assert.equal('origin.asn.spameatingmonkey.net', zone);
         assert.equal('15169', obj.asn);
         assert.equal('8.8.8.0/24', obj.net);
       }
@@ -169,77 +164,75 @@ describe('get_dns_results', function () {
   })
 })
 
-describe.skip('maxmind geoip db v1', () => {
-  /* DEAD: MaxMind no longer publishes updates */
+describe('lookup_via_dns', function () {
+  it('returns results from active providers', function (done) {
+    const asn = new fixtures.plugin('asn');
+    asn.cfg = { main: { }, protocols: { dns: true } };
+    const connection = fixtures.connection.createConnection();
+    connection.remote.ip='66.128.51.163';
+
+    asn.test_and_register_dns_providers().then((providers) => {
+      asn.lookup_via_dns((r) => {
+        assert.ok(r.length)
+        done()
+      },
+      connection)
+    })
+  })
+})
+
+describe('maxmind geoip db', () => {
+
   it('test_and_register_geoip', (done) => {
     const asn = new fixtures.plugin('asn');
     asn.cfg = { main: { }, protocols: { geoip: true } };
-    asn.test_and_register_geoip();
-    assert.ok(asn.maxmind);
-    done();
+    asn.test_and_register_geoip().then((r) => {
+      // console.log(r)
+      assert.ok(asn.maxmind)
+      // assert.ok(asn.ASNLookup)
+      // console.log(asn.ASNLookup)
+      done()
+    })
   })
-
 
   it('lookup_via_maxmind, IPv4', (done) => {
     const asn = new fixtures.plugin('asn');
     asn.cfg = { main: { }, protocols: { geoip: true } };
     asn.connection = fixtures.connection.createConnection();
     asn.connection.remote.ip='8.8.8.8';
-    asn.test_and_register_geoip();
-
-    asn.lookup_via_maxmind(() => {
-      if (asn.mmDbsAvail && asn.mmDbsAvail.length > 0) {
-        const res = asn.connection.results.get('asn');
-        assert.equal(res.asn, 15169);
-        assert.equal(res.org, 'Google LLC');
-      }
-      else {
-        console.error('no DBs found');
-      }
-      done();
-    },
-    asn.connection);
+    asn.test_and_register_geoip().then(() => {
+      asn.lookup_via_maxmind(() => {
+        if (asn.dbsLoaded) {
+          const res = asn.connection.results.get('asn');
+          assert.equal(res.asn, 15169);
+          assert.equal(res.org, 'GOOGLE');
+        }
+        else {
+          console.error('no DBs found');
+        }
+        done();
+      },
+      asn.connection);
+    })
   })
-
-  it('lookup_via_maxmind, IPv6', (done) => {
-    const asn = new fixtures.plugin('asn');
-    asn.cfg = { main: { }, protocols: { geoip: true } };
-    asn.connection = fixtures.connection.createConnection();
-    asn.connection.remote.ip='2001:4860:4860::8888';
-    asn.test_and_register_geoip();
-
-    asn.lookup_via_maxmind(() => {
-      console.log(`dbs: ${asn.mmDbsAvail}`)
-      if (asn.mmDbsAvail && asn.mmDbsAvail.length > 0) {
-        const res = asn.connection.results.get('asn');
-        // console.log(res);
-        assert.equal(res.asn, 15169);
-        assert.equal(res.org, 'Google LLC');
-      }
-      else {
-        console.error('no DBs found');
-      }
-      done();
-    },
-    asn.connection);
-  });
 
   it('maxmind AS w/o org', (done) => {
     const asn = new fixtures.plugin('asn');
     asn.cfg = { main: { }, protocols: { geoip: true } };
     asn.connection = fixtures.connection.createConnection();
     asn.connection.remote.ip='216.255.64.1';
-    asn.test_and_register_geoip();
+    asn.test_and_register_geoip().then(() => {
 
-    asn.lookup_via_maxmind(() => {
-      if (asn.mmDbsAvail && asn.mmDbsAvail.length > 0) {
-        const res = asn.connection.results.get('asn');
-        // console.log(res);
-        assert.equal(res.asn, 63200);
-        assert.equal(res.org, '');
-      }
-      done();
-    },
-    asn.connection);
-  });
+      asn.lookup_via_maxmind(() => {
+        if (asn.mmDbsAvail && asn.mmDbsAvail.length > 0) {
+          const res = asn.connection.results.get('asn');
+          // console.log(res);
+          assert.equal(res.asn, 63200);
+          assert.equal(res.org, '');
+        }
+        done();
+      },
+      asn.connection);
+    })
+  })
 })
