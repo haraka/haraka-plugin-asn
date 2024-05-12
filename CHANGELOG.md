9a7c19fb85c2d7b8bab68f09ae0fe5b096066a07
Matt Simerson <matt@tnpi.net>
2024-05-12 12:02:10 -0700

    doc(CONTRIBUTORS): added
    - populate [files] in package.json
    - dep: eslint-plugin-haraka -> @haraka/eslint-config
    - lint: remove duplicate / stale rules from .eslintrc
    - doc: renamed Changes.md -> CHANGELOG.md
    - prettier
    - test: increase timeout for DNS results
    - chore: remove old config compat shim

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,21 @@
+# Changelog
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/).
+
 ### Unreleased
 
+### [2.0.3] - 2023-12-13
+
+- ci: use shared ci workflows
+- confirm maxmind db has loaded before registering hook #23
+- populate [files] in package.json
+- dep: eslint-plugin-haraka -> @haraka/eslint-config
+- lint: remove duplicate / stale rules from .eslintrc
+- doc: renamed Changes.md -> CHANGELOG.md
 
 ### 2.0.2 - 2023-02-07
 
 - fix: catching DNS timeout exception
-
 
 ### 2.0.1 - 2022-05-27
 
@@ -12,14 +23,12 @@
 - fix: when adding header, look in correct location for asn.org
 - when create conn note, only assign properties with values
 
-
 ### 2.0.0 - 2022-05-23
 
 - style: replace most callbacks with async/await
 - use builtin/promises where available
 - asn.ini: switch default dns provider to rspamd
 - dep: remove async
-
 
 ### 1.0.9 - 2022-05-22
 
@@ -30,7 +39,6 @@
 - style: more es6/7
 - dep(async): bump version to 3.2
 
-
 ### 1.0.8 - 2018-01-22
 
 - parse maxmind ASN w/o Org data
@@ -39,18 +47,15 @@
 - added parse_rspamd test
 - emit rspamd DNS provider results (when enabled)
 
-
 ### 1.0.7 - 2017-02-06
 
 - updated eslint to use eslint-plugin-haraka
 - aggregate results before emitting
 
-
 ### 1.0.6 - 2016-10-20
 
-* when protocols[setting]=false, don't enable that protocol
-    * ie, do what the config implies
-
+- when protocols[setting]=false, don't enable that protocol
+  - ie, do what the config implies
 
 ### 1.0.5 - 2016-10-08
 
@@ -62,5 +67,8 @@
 
 ### 1.0.0 - 2016-07-21
 
-
-[2.0.2]: https://github.com/haraka/haraka-plugin-asn/releases/tag/2.0.2
+[1.0.9]: https://github.com/haraka/haraka-plugin-asn/releases/tag/v1.0.9
+[2.0.0]: https://github.com/haraka/haraka-plugin-asn/releases/tag/v2.0.0
+[2.0.1]: https://github.com/haraka/haraka-plugin-asn/releases/tag/2.0.1
+[2.0.2]: https://github.com/haraka/haraka-plugin-asn/releases/tag/v2.0.2
+[2.0.3]: https://github.com/haraka/haraka-plugin-asn/releases/tag/2.0.3
--- index.js
+++ index.js
@@ -1,232 +1,237 @@
 // determine the ASN of the connecting IP
 
-// node built-ins
-const dns     = require('dns').promises;
-const fs      = require('fs/promises');
-const path    = require('path')
+const dns = require('node:dns').promises
+const fs = require('node:fs/promises')
+const path = require('node:path')
 
-let test_ip   = '66.128.51.163';
-const providers = [];
-let conf_providers = [ 'origin.asn.cymru.com', 'asn.routeviews.org', 'asn.rspamd.com' ];
+let test_ip = '66.128.51.163'
+const providers = []
+let conf_providers = [
+  'origin.asn.cymru.com',
+  'asn.routeviews.org',
+  'asn.rspamd.com',
+]
 
 exports.register = async function () {
-  this.registered = false;
+  this.registered = false
 
-  this.load_asn_ini();
+  this.load_asn_ini()
 
-  await this.test_and_register_dns_providers();
-  await this.test_and_register_geoip();
+  await this.test_and_register_dns_providers()
+  await this.test_and_register_geoip()
 
   if (this.cfg.header.asn) {
-    this.register_hook('data_post', 'add_header_asn');
+    this.register_hook('data_post', 'add_header_asn')
   }
   if (this.cfg.header.provider) {
-    this.register_hook('data_post', 'add_header_provider');
+    this.register_hook('data_post', 'add_header_provider')
   }
 }
 
 exports.test_and_register_dns_providers = async function () {
-  if (!this.cfg.protocols.dns) return; // disabled in config
+  if (!this.cfg.protocols.dns) return // disabled in config
 
   for (const zone of conf_providers) {
     try {
       const res = await this.get_dns_results(zone, test_ip)
       if (!res) {
-        this.logerror(this, `${zone} failed`);
-        continue;
+        this.logerror(this, `${zone} failed`)
+        continue
       }
 
-      this.logdebug(this, `${zone} succeeded`);
+      this.logdebug(this, `${zone} succeeded`)
 
-      if (!providers.includes(zone)) providers.push(zone);
-      if (this.registered) continue;
-      this.registered = true;
-      this.register_hook('lookup_rdns', 'lookup_via_dns');
-    }
-    catch (err) {
-      this.logerror(this, `zone ${zone} encountered ${err.message}`);
+      if (!providers.includes(zone)) providers.push(zone)
+      if (this.registered) continue
+      this.registered = true
+      this.register_hook('lookup_rdns', 'lookup_via_dns')
+    } catch (err) {
+      this.logerror(this, `zone ${zone} encountered ${err.message}`)
     }
   }
   return providers
 }
 
 exports.load_asn_ini = function () {
-  const plugin = this;
-  plugin.cfg = plugin.config.get('asn.ini', {
-    booleans: [
-      '+header.asn',
-      '-header.provider',
-      '+protocols.dns',
-      '+protocols.geoip',
-    ]
-  },
-  function () {
-    plugin.load_asn_ini();
-  });
+  const plugin = this
+  plugin.cfg = plugin.config.get(
+    'asn.ini',
+    {
+      booleans: [
+        '+header.asn',
+        '-header.provider',
+        '+protocols.dns',
+        '+protocols.geoip',
+      ],
+    },
+    function () {
+      plugin.load_asn_ini()
+    },
+  )
 
-  const c = plugin.cfg;
+  const c = plugin.cfg
   if (c.main.providers !== undefined) {
     if (c.main.providers === '') {
-      conf_providers = [];
-    }
-    else {
-      conf_providers = c.main.providers.split(/[\s,;]+/);
+      conf_providers = []
+    } else {
+      conf_providers = c.main.providers.split(/[\s,;]+/)
     }
   }
 
-  if (c.main.test_ip) test_ip = c.main.test_ip;
-
-  // backwards compat with old config settings (Sunset 3.0)
-  if (c.main.asn_header !== undefined) c.header.asn = c.main.asn_header;
-  if (c.main.provider_header !== undefined) c.header.provider = c.main.provider_header;
+  if (c.main.test_ip) test_ip = c.main.test_ip
 }
 
 exports.get_dns_results = async function (zone, ip) {
-  const query = `${ip.split('.').reverse().join('.')}.${zone}`;
+  const query = `${ip.split('.').reverse().join('.')}.${zone}`
 
   const timeout = (prom, time, exception) => {
-    let timer;
+    let timer
     return Promise.race([
       prom,
-      new Promise((_r, rej) => timer = setTimeout(rej, time, exception))
-    ]).finally(() => clearTimeout(timer));
+      new Promise((_r, rej) => (timer = setTimeout(rej, time, exception))),
+    ]).finally(() => clearTimeout(timer))
   }
 
   try {
     const addrs = await timeout(
       dns.resolveTxt(query),
       (this.cfg.main.timeout || 4) * 1000,
-      new Error(`${zone} timeout`)
-    );
+      new Error(`${zone} timeout`),
+    )
 
     if (!addrs || !addrs[0]) {
-      this.logerror(this, `no results for ${query}`);
+      this.logerror(this, `no results for ${query}`)
       return
     }
 
-    const first = addrs[0];
+    const first = addrs[0]
 
-    this.logdebug(this, `${zone} answers: ${first}`);
+    this.logdebug(this, `${zone} answers: ${first}`)
 
-    return this.get_result(zone, first);
+    return this.get_result(zone, first)
+  } catch (err) {
+    this.logerror(this, `error: ${err} running: ${query}`)
   }
-  catch (err) {
-    this.logerror(this, `error: ${err} running: ${query}`);
-  }
-
 }
 
 exports.get_result = function (zone, first) {
-
   switch (zone) {
-    case 'origin.asn.cymru.com'           : return this.parse_cymru(first.join(''));
-    case 'asn.routeviews.org'             : return this.parse_routeviews(first);
-    case 'asn.rspamd.com'                 : return this.parse_rspamd(first.join(''));
-    case 'origin.asn.spameatingmonkey.net': return this.parse_monkey(first.join(''));
+    case 'origin.asn.cymru.com':
+      return this.parse_cymru(first.join(''))
+    case 'asn.routeviews.org':
+      return this.parse_routeviews(first)
+    case 'asn.rspamd.com':
+      return this.parse_rspamd(first.join(''))
+    case 'origin.asn.spameatingmonkey.net':
+      return this.parse_monkey(first.join(''))
   }
 
-  this.logerror(this, `unrecognized ASN provider: ${zone}`);
-  return;
+  this.logerror(this, `unrecognized ASN provider: ${zone}`)
+  return
 }
 
 exports.lookup_via_dns = function (next, connection) {
-  const plugin = this;
-  if (connection.remote.is_private) return next();
+  if (connection.remote.is_private) return next()
 
   const promises = []
 
   for (const zone of providers) {
-    promises.push(new Promise(resolve => {
-      // connection.logdebug(plugin, `zone: ${zone}`);
+    promises.push(
+      new Promise((resolve) => {
+        // connection.logdebug(plugin, `zone: ${zone}`);
 
-      try {
+        try {
+          this.get_dns_results(zone, connection.remote.ip).then((r) => {
+            if (!r) return resolve()
 
-        plugin.get_dns_results(zone, connection.remote.ip).then(r => {
-          if (!r) return resolve();
+            const results = { emit: true }
 
-          const results = { emit: true };
+            // store asn & net from any source
+            if (r.asn) results.asn = r.asn
+            if (r.net) results.net = r.net
 
-          // store asn & net from any source
-          if (r.asn) results.asn = r.asn;
-          if (r.net) results.net = r.net;
+            // store provider specific results
+            switch (zone) {
+              case 'origin.asn.cymru.com':
+                results.cymru = r
+                break
+              case 'asn.routeviews.org':
+                results.routeviews = r
+                break
+              case 'origin.asn.spameatingmonkey.net':
+                results.monkey = r
+                break
+              case 'asn.rspamd.com':
+                results.rspamd = r
+                break
+            }
 
-          // store provider specific results
-          switch (zone) {
-            case 'origin.asn.cymru.com':
-              results.cymru = r;
-              break;
-            case 'asn.routeviews.org':
-              results.routeviews = r;
-              break;
-            case 'origin.asn.spameatingmonkey.net':
-              results.monkey = r;
-              break;
-            case 'asn.rspamd.com':
-              results.rspamd = r;
-              break;
-          }
-
-          connection.results.add(plugin, results);
-          resolve(results);
-        })
-      }
-      catch (err) {
-        connection.results.add(plugin, { err })
-        resolve();
-      }
-    }))
+            connection.results.add(this, results)
+            resolve(results)
+          })
+        } catch (err) {
+          connection.results.add(this, { err })
+          resolve()
+        }
+      }),
+    )
   }
 
   Promise.all(promises).then(next)
 }
 
 exports.parse_routeviews = function (thing) {
-  let labels;
+  let labels
 
   if (typeof thing === 'string' && /,/.test(thing)) {
-    labels = thing.split(',');
-    return { asn: labels[0], net: `${labels[1]}/${labels[2]}` };
+    labels = thing.split(',')
+    return { asn: labels[0], net: `${labels[1]}/${labels[2]}` }
   }
 
   // this is a correct result (node >= 0.10.26)
   // 99.177.75.208.asn.routeviews.org. IN TXT "40431" "208.75.176.0" "21"
   if (Array.isArray(thing)) {
-    labels = thing;
-  }
-  else {
+    labels = thing
+  } else {
     // this is what node (< 0.10.26) returns
     // 99.177.75.208.asn.routeviews.org. IN TXT "40431208.75.176.021"
-    labels = thing.split(/ /);
+    labels = thing.split(/ /)
   }
 
   if (labels.length !== 3) {
-    this.logerror(this, `result length not 3: ${labels.length} string="${thing}"`);
-    return;
+    this.logerror(
+      this,
+      `result length not 3: ${labels.length} string="${thing}"`,
+    )
+    return
   }
 
-  return { asn: labels[0], net: `${labels[1]}/${labels[2]}` };
+  return { asn: labels[0], net: `${labels[1]}/${labels[2]}` }
 }
 
 exports.parse_cymru = function (str) {
-  const r = str.split(/\s+\|\s*/);
+  const r = str.split(/\s+\|\s*/)
   //  99.177.75.208.origin.asn.cymru.com. 14350 IN TXT
   //        "40431 | 208.75.176.0/21 | US | arin | 2007-03-02"
   //        "10290 | 12.129.48.0/24  | US | arin |"
   if (r.length < 4) {
-    this.logerror(this, `cymru: bad result length ${r.length} string="${str}"`);
-    return;
+    this.logerror(this, `cymru: bad result length ${r.length} string="${str}"`)
+    return
   }
-  return { asn: r[0], net: r[1], country: r[2], assignor: r[3], date: r[4] };
+  return { asn: r[0], net: r[1], country: r[2], assignor: r[3], date: r[4] }
 }
 
 exports.parse_monkey = function (str) {
-  const plugin = this;
-  const r = str.split(/\s+\|\s+/);
+  const plugin = this
+  const r = str.split(/\s+\|\s+/)
   // "74.125.44.0/23 | AS15169 | Google Inc. | 2000-03-30"
   // "74.125.0.0/16 | AS15169 | Google Inc. | 2000-03-30 | US"
   if (r.length < 3) {
-    plugin.logerror(plugin, `monkey: bad result length ${r.length} string="${str}"`);
-    return;
+    plugin.logerror(
+      plugin,
+      `monkey: bad result length ${r.length} string="${str}"`,
+    )
+    return
   }
   return {
     asn: r[1].substring(2),
@@ -234,81 +239,81 @@ exports.parse_monkey = function (str) {
     org: r[2],
     date: r[3],
     country: r[4],
-  };
+  }
 }
 
 exports.parse_rspamd = function (str) {
-  const plugin = this;
-  const r = str.split(/\s*\|\s*/);
+  const plugin = this
+  const r = str.split(/\s*\|\s*/)
   //  8.8.8.8.asn.rspamd.com. 14350 IN TXT
   //        "15169|8.8.8.0/24|US|arin|"
 
   if (r.length < 4) {
-    plugin.logerror(plugin, `rspamd: bad result length ${r.length} string="${str}"`);
-    return;
+    plugin.logerror(
+      plugin,
+      `rspamd: bad result length ${r.length} string="${str}"`,
+    )
+    return
   }
-  return { asn: r[0], net: r[1], country: r[2], assignor: r[3], date: r[4] };
+  return { asn: r[0], net: r[1], country: r[2], assignor: r[3], date: r[4] }
 }
 
 exports.add_header_asn = function (next, connection) {
-
-  const asn = connection.results.get('asn');
-  if (!asn?.asn) return next();
-  if (!connection.transaction) return next();
+  const asn = connection.results.get('asn')
+  if (!asn?.asn) return next()
+  if (!connection.transaction) return next()
 
   if (asn.net) {
-    connection.transaction.add_header('X-Haraka-ASN', `${asn.asn} ${asn.net}`);
-  }
-  else {
-    connection.transaction.add_header('X-Haraka-ASN', `${asn.asn}`);
+    connection.transaction.add_header('X-Haraka-ASN', `${asn.asn} ${asn.net}`)
+  } else {
+    connection.transaction.add_header('X-Haraka-ASN', `${asn.asn}`)
   }
   if (asn.org) {
-    connection.transaction.add_header('X-Haraka-ASN-Org', `${asn.org}`);
+    connection.transaction.add_header('X-Haraka-ASN-Org', `${asn.org}`)
   }
 
-  next();
+  next()
 }
 
 exports.add_header_provider = function (next, connection) {
-
-  const asn = connection.results.get('asn');
-  if (!asn?.asn) return next();
+  const asn = connection.results.get('asn')
+  if (!asn?.asn) return next()
 
   for (const p in asn) {
-    if (!asn[p].asn) continue;  // ignore non-object results
+    if (!asn[p].asn) continue // ignore non-object results
 
-    const name = `X-Haraka-ASN-${p.toUpperCase()}`;
-    const values = [];
+    const name = `X-Haraka-ASN-${p.toUpperCase()}`
+    const values = []
     for (const k in asn[p]) {
-      values.push(`${k}=${asn[p][k]}`);
+      values.push(`${k}=${asn[p][k]}`)
     }
-    if (values.length === 0) continue;
-    connection.transaction.add_header(name, values.join(' '));
+    if (values.length === 0) continue
+    connection.transaction.add_header(name, values.join(' '))
   }
 
-  next();
+  next()
 }
 
 exports.test_and_register_geoip = async function () {
-  if (!this.cfg.protocols.geoip) return; // disabled in config
+  if (!this.cfg.protocols.geoip) return // disabled in config
 
   try {
-    this.maxmind = require('maxmind');
+    this.maxmind = require('maxmind')
     if (await this.load_dbs()) {
-      this.register_hook('connect', 'lookup_via_maxmind');
+      this.register_hook('connect', 'lookup_via_maxmind')
     }
-  }
-  catch (e) {
-    this.logerror(e);
-    this.logerror("unable to load maxmind, try\n\n\t'npm install -g maxmind@0.6'\n\n");
+  } catch (e) {
+    this.logerror(e)
+    this.logerror(
+      "unable to load maxmind, try\n\n\t'npm install -g maxmind@0.6'\n\n",
+    )
   }
 }
 
 exports.load_dbs = async function () {
-
-  this.dbsLoaded = 0;
-  const dbdir = this.cfg.main.dbdir || '/usr/local/share/GeoIP/';
-  const dbPath = path.join(dbdir, `GeoLite2-ASN.mmdb`);
+  this.dbsLoaded = 0
+  const dbdir = this.cfg.main.dbdir || '/usr/local/share/GeoIP/'
+  const dbPath = path.join(dbdir, `GeoLite2-ASN.mmdb`)
 
   try {
     await fs.access(dbPath)
@@ -318,14 +323,13 @@ exports.load_dbs = async function () {
       watchForUpdates: true,
       cache: {
         max: 1000, // max items in cache
-        maxAge: 1000 * 60 * 60 // life time in milliseconds
-      }
+        maxAge: 1000 * 60 * 60, // life time in milliseconds
+      },
     })
 
-    this.loginfo(`loaded maxmind db ${dbPath}`);
-    this.dbsLoaded++;
-  }
-  catch (e) {
+    this.loginfo(`loaded maxmind db ${dbPath}`)
+    this.dbsLoaded++
+  } catch (e) {
     console.error(e)
     this.loginfo(`missing [access to] DB ${dbPath}`)
   }
@@ -334,15 +338,19 @@ exports.load_dbs = async function () {
 }
 
 exports.lookup_via_maxmind = function (next, connection) {
-  if (!this.maxmind || !this.dbsLoaded) return next();
+  if (!this.maxmind || !this.dbsLoaded) return next()
 
   const asn = this.lookup.get(connection.remote.ip)
   if (asn?.autonomous_system_number || asn?.autonomous_system_organization) {
     connection.results.add(this, {
-      ...(asn.autonomous_system_number       ? { asn: asn.autonomous_system_number }       : {}),
-      ...(asn.autonomous_system_organization ? { org: asn.autonomous_system_organization } : {}),
-    });
+      ...(asn.autonomous_system_number
+        ? { asn: asn.autonomous_system_number }
+        : {}),
+      ...(asn.autonomous_system_organization
+        ? { org: asn.autonomous_system_organization }
+        : {}),
+    })
   }
 
-  next();
+  next()
 }
